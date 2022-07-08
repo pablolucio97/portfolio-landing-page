ea59b57d7d809ae0cd2e8461362c07089713a213
Pablo <pablolucio_@hotmail.com>
2022-07-08 11:45:58 -0300

    IntroductionSection created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,54 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Container } from '../styles'
+import { SubTitle } from '../components/Typography/SubTitle'
+import { Title } from '../components/Typography/Title'
+import { Text } from '../components/Typography/Text'
+import {
+  Container,
+  IntroductionSection,
+  IntroductionSectionContainer,
+  IntroductionSectionContentContainer,
+  IntroductionSectionContentInfoContainer,
+  IntroductionSectionImageContainer
+} from '../styles'
+import { NextImage } from '../components/Next/NextImage'
 
 const Home: NextPage = () => {
 
   return (
-    <Container style={{marginBottom: 120}}>
+    <Container style={{ marginBottom: 120 }}>
       <Head>
-        <title> MyPage | Home</title>
+        <title> PabloSilvaDev | Portfólio</title>
         {/* @ts-ignore */}
         <meta charset="UTF-8" />
         <meta name="description" content="Landing pages de alta conversão e sites institucionais para o seu negócio." />
         <meta name="keywords" content="pablosilvadev pablo silva psd landing pages" />
         <meta name='author' content='Pablo Silva' />
-        <meta name="theme-color" content="#000000" />
       </Head>
+      <IntroductionSection>
+        <IntroductionSectionContainer>
+          <Title
+            content='Quem sou eu'
+          />
+          <IntroductionSectionContentContainer>
+            <IntroductionSectionContentInfoContainer>
+              <SubTitle
+                content='Olá!'
+              />
+              <Text
+                content='Meu nome é Pablo Silva, sou programador com 2 anos de experiência na criação de sites e aplicativos utilzando tecnologias modernas e altamente performáticas que ajudarão você ou sua empresa a  desenvolver projetos para o mundo virtual. O meu foco é construir aplicações inteligentes que fazem o uso de componentes que reforçam um padrão de projeto.'
+              />
+            </IntroductionSectionContentInfoContainer>
+            <IntroductionSectionImageContainer>
+              <NextImage
+                height={280}
+                width={280}
+                imgUrl='/profile.png'
+              />
+            </IntroductionSectionImageContainer>
+          </IntroductionSectionContentContainer>
+        </IntroductionSectionContainer>
+      </IntroductionSection>
     </Container>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -32,12 +32,94 @@ export const BannerSectionContainer = styled.div`
   background-size: cover;
 `; */
 
-export const BannerSection = styled.section`
-  display: flex;
-`;
 
 export const IntroductionSection = styled.section`
   display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 64vh;
+  background: -webkit-linear-gradient(-41deg, rgb(5, 5, 5), rgb(31, 31, 31));
+  background: linear-gradient(-41deg, rgb(5, 5, 5), rgb(31, 31, 31));
+
+`;
+
+export const IntroductionSectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 1900px;
+  margin: 0 auto;
+  padding: 40px;
+  & h1 {
+    color: ${({ theme }) => theme.colors.white100};
+    margin: 0 auto;
+    font-weight: 800;
+  }
+`
+export const IntroductionSectionContentContainer = styled.div`
+  display: flex;
+ 
+  align-items: center;
+  width: 100%;
+  margin: 0 auto;
+  & h1 {
+    color: ${({ theme }) => theme.colors.white100};
+    margin: 0 auto;
+    font-weight: 800;
+  }
+
+  @media (max-width: 720px){
+    flex-direction: column;
+  }
+`
+
+export const IntroductionSectionContentInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 60%;
+
+  & h3 {
+    color: ${({ theme }) => theme.colors.white100};
+    margin-top: 24px;
+    margin-bottom: -12px;
+  }
+  & p {
+    color: ${({ theme }) => theme.colors.white500};
+    margin: 24px auto;
+    font-weight: 200;
+  }
+
+  @media (max-width: 720px){
+    align-items: center;
+    width: 100%;
+  }
+
+  /* @media (max-width: 1080px) {
+    width: 100%;
+    padding: 16px;
+    & h1 {
+      text-align: center;
+    }
+  }
+  @media (max-width: 480px) {
+    width: 100%;
+    padding: 0;
+    & h1 {
+      text-align: center;
+    }
+  } */
+`;
+
+export const IntroductionSectionImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40%;
+  padding: 40px;
+  @media (max-width: 1080px) {
+    width: 80%;
+  }
 `;
 
 export const MetricsSection = styled.section`
@@ -56,13 +138,8 @@ export const PurchaseSection = styled.section`
   display: flex;
 `;
 
-export const BannerSectionContainer = styled.div`
-  display: flex;
-`;
 
-export const IntroductionSectionContainer = styled.div`
-  display: flex;
-`;
+
 
 export const MetricsSectionContainer = styled.div`
   display: flex;
